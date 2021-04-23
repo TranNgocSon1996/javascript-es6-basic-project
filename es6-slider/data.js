3c9c97b3b8958407b52b73f2a20084af8b6e9433
TranNgocSon1996 <transon821996@gmail.com>
2021-04-23 11:11:22 +0700

    upload html + css code

---
+++ es6-slider/data.js
@@ -0,0 +1,23 @@
+const people = [
+    {
+      img:
+        "https://res.cloudinary.com/loveviruss/image/upload/v1618841332/samples/people/boy-snow-hoodie.jpg",
+      name: "peter doe",
+      job: "product manager",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+    },
+    {
+      img:
+        "https://res.cloudinary.com/loveviruss/image/upload/v1618841594/samples/people/bicycle.jpg",
+      name: "susan doe",
+      job: "developer",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+    {
+      img:
+        "https://res.cloudinary.com/loveviruss/image/upload/v1618841334/samples/landscapes/beach-boat.jpg",
+      name: "emma doe",
+      job: "designer",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+  ];
